e90a044339c3f9b120b17aa0319621a9b0c34362
Deya Genao <deyagenao@gmail.com>
2020-04-16 00:08:49 -0400

    added all images for project section

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,6 +1,6 @@
 // function for making project description visibile on hover
 $(".project").hover(function(){
-  var projectNum = $(this).data("num");
+  // var projectNum = $(this).data("num");
   $(".project-description").show();
 }, function() {
   $(".project-description").hide();
